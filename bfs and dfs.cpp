5b39c1d22c02f84eb373f4a43387aef9aa2e0036
Saichetan1111 <saichetan1111@gmail.com>
2023-06-28 14:10:28 +0530

    dijistra

--- bfs and dfs.cpp
+++ bfs and dfs.cpp
@@ -1,4 +1,7 @@
-/* DFS */
+/* DFS 
+  TC O(V+E)
+  SC O(V)auxilary stack space+O(V)
+*/
 void dfs(int node ,int par)
 {
     for(auto it:adj[node])
@@ -6,7 +9,10 @@ void dfs(int node ,int par)
         if(it!=par)dfs(it,node);
     }
 }
-/* BFS */
+/* BFS 
+   TC O(V+E)
+   SC O(V)
+*/
 void bfs(int node)
 {
     queue<int>q;
@@ -27,7 +33,10 @@ void bfs(int node)
         }
     }
 }
-/* DIJKISTRA */
+/* DIJKISTRA 
+  TC O(V+Elog(E))
+  SC O(V);
+*/
 vi dij(vector<vector<pair<int,int>>>adj,int node)
 {
 	vi ans(adj.size(),1e9);
@@ -38,6 +47,9 @@ vi dij(vector<vector<pair<int,int>>>adj,int node)
 	{
 		pair<int,int>p=pq.top();
 		pq.pop();
+        if(p.ff>ans[p.ss]){continue;}
+        /*THERE IS A WAY TO REACH NODE P IN SMALLER DISTANCE.
+        SO SKIP IT.*/
 		for(auto it:adj[p.ss])
 		{
 			if(ans[it.ff]>p.ff+it.ss)
